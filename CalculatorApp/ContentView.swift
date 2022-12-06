b33076869b89625bfcab4f413e1d69d3c0e4cf68
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2022-12-06 03:43:20 -0500

    Added a body and background layout for app

--- CalculatorApp/ContentView.swift
+++ CalculatorApp/ContentView.swift
@@ -23,16 +23,61 @@ struct ContentView: View
        @State var result = ""
        @State var alert = false
     
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    var body: some View
+    {
+        VStack
+        {
+            HStack
+            {
+                Spacer()
+                Text(working)
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 30, weight: .heavy))
+            }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            HStack
+            {
+                Spacer()
+                Text(result)
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 50, weight: .heavy))
+            }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            ForEach(grid, id: \.self)
+            {
+                row in
+                HStack
+                {
+                    ForEach(row, id: \.self)
+                    {
+                        cell in
+                        
+                        Button(action: { buttonPressed(cell: cell)}, label: {
+                            Text(cell)
+                                .foregroundColor(buttonColor(cell))
+                                .font(.system(size: 40, weight: .heavy))
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            
+                        })
+                        
+                    }
+                }
+            }
+            
+            
+            
         }
-        .padding()
+        .background(Color.black.ignoresSafeArea())
+        .alert(isPresented: $alert)
+        {
+            Alert(
+                title: Text("Invalid Input"),
+                message: Text(working),
+                dismissButton: .default(Text("Okay"))
+            )
+        }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
