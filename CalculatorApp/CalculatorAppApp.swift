ebc36b87fa488bc42c07f029584749fd2f4e5d0a
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2022-12-06 03:36:44 -0500

    First Commit, Added project to Github

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Shivam Bajaj on 2022-12-06.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
